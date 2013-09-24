c582c5d33546b82051c861054ae0122472bf8c7d
turlir <iljaosincev@gmail.com>
2013-09-24 19:26:39 +0600

    rename directory name

--- src/ru/example/ursmu/Abstraction/IDownloadBehavior.java
+++
@@ -1,7 +0,0 @@
-package ru.example.ursmu.Abstraction;
-
-import java.io.IOException;
-
-public interface IDownloadBehavior {
-    public String Download(String uri, String parameters) throws IOException;
-}
--- src/ru/example/ursmu/Abstraction/UniversalCallback.java
+++
@@ -1,9 +0,0 @@
-package ru.example.ursmu.Abstraction;
-
-public interface UniversalCallback {
-    public void sendError(String notify);
-
-    public void sendComplete(Object[] data);
-
-    public void sendStart();
-}
