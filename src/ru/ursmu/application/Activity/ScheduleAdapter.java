ea677af890e6ecdc4e3807785071c6e3d6de4bc7
turlir <iljaosincev@gmail.com>
2013-10-22 00:26:13 +0600

    package rename

--- src/ru/ursmu/application/Activity/ScheduleAdapter.java
+++ src/ru/ursmu/application/Activity/ScheduleAdapter.java
@@ -10,7 +10,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import ru.ursmu.application.JsonObject.EducationItem;
-import ru.ursmu.application.R;
+import ru.ursmu.beta.application.R;
 
 import java.lang.ref.WeakReference;
 import java.util.Calendar;
